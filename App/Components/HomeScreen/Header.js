e83b3e48a995c7daab26e723de3c76fc66ddfa22
Resources-DTW <resource.dtw@gmail.com>
2023-12-30 15:55:18 +0530

    30/12 commit

--- App/Components/HomeScreen/Header.js
+++ App/Components/HomeScreen/Header.js
@@ -1,35 +1,52 @@
-import { View, Text, Image, StyleSheet } from "react-native";
+import { View, Text, Image, StyleSheet, TextInput } from "react-native";
 import React from "react";
 import { useUser } from "@clerk/clerk-expo";
 import Colors from "../../Utils/Colors";
 import Coin from "../../../assets/images/coin.png";
+import { Ionicons } from "@expo/vector-icons";
 
 const Header = () => {
   const { isLoaded, isSignedIn, user } = useUser();
 
   return (
     isLoaded && (
-      <View style={[styles.rowStyle, { justifyContent: "space-between" }]}>
-        <View style={styles.rowStyle}>
-          <Image
-            source={{ uri: user?.imageUrl }}
-            resizeMode="contain"
-            style={{ width: 50, height: 50, borderRadius: 99 }}
-          />
-          <View>
-            <Text style={{ color: Colors.WHITE, fontFamily: "outfit" }}>
-              Welcome,
-            </Text>
-            <Text style={styles.mainText}>{user?.fullName}</Text>
+      <View>
+        <View style={[styles.rowStyle, { justifyContent: "space-between" }]}>
+          <View style={styles.rowStyle}>
+            <Image
+              source={{ uri: user?.imageUrl }}
+              resizeMode="contain"
+              style={{ width: 50, height: 50, borderRadius: 99 }}
+            />
+            <View>
+              <Text style={{ color: Colors.WHITE, fontFamily: "outfit" }}>
+                Welcome,
+              </Text>
+              <Text style={styles.mainText}>{user?.fullName}</Text>
+            </View>
+          </View>
+          <View style={styles.rowStyle}>
+            <Image
+              source={Coin}
+              resizeMode="contain"
+              style={{ width: 35, height: 35 }}
+            />
+            <Text style={styles.mainText}>6969</Text>
           </View>
         </View>
-        <View style={styles.rowStyle}>
-          <Image
-            source={Coin}
-            resizeMode="contain"
-            style={{ width: 35, height: 35 }}
+        <View
+          style={{
+            backgroundColor: Colors.WHITE,
+            paddingLeft: 20,
+            flexDirection: "row",
+            justifyContent: "space-between",
+          }}
+        >
+          <TextInput
+            placeholder="Search Courses"
+            style={{ fontFamily: "outfit", fontSize: 18 }}
           />
-          <Text style={styles.mainText}>6969</Text>
+          <Ionicons name="search-circle" size={50} color={Colors.PRIMARY} />
         </View>
       </View>
     )
